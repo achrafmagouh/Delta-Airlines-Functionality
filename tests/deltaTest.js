05b97b69f4b385a408c43a6b33a6cf13c3d8f36e
keenanSchramm <keenan.schramm@gmail.com>
2019-08-21 10:52:57 -0600

    to from & selectors

--- tests/deltaTest.js
+++ tests/deltaTest.js
@@ -8,6 +8,16 @@ module.exports = {
         browser.end()
     },
     'Test1': browser =>{
+        delta
+            .click('@fromAirport')
+            .setValue('@cityInput', 'SGU')
+            .click('@searchExample')
+            .verify.containsText('@fromAirport', 'SGU')
+            .click('@toAirport')
+            .setValue('@cityInput', 'SLC')
+            .click('@searchExample')
+            .verify.containsText('@toAirport', 'SLC')
+            .pause(2000)
         
     }
 
